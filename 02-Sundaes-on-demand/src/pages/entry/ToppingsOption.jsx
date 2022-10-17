e642fedd89c6d6a54aacd93f817f53367964d05d
Oshoke <oyatioshoke@yahoo.com>
2022-10-17 22:09:34 +0100

    more test lectures

---
+++ 02-Sundaes-on-demand/src/pages/entry/ToppingsOption.jsx
@@ -0,0 +1,15 @@
+import Col from 'react-bootstrap/col';
+
+export default function ToppingsOption({ name, imagePath }) {
+  return (
+    <div>
+      <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+        <img
+          style={{ width: '75%' }}
+          alt={`${name} topping`}
+          src={`http://localhost3030${imagePath}`}
+        />
+      </Col>
+    </div>
+  );
+}
---
+++ 02-Sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import Options from '../options';
+
+// for scoops
+
+test('displays image for each scoop option from the server', async () => {
+  render(<Options optionType='scoops' />);
+
+  // find images
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm the alt text of images
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+// for toppings
+
+test('displays image for each toppings from the server', async () => {
+  render(<Options optionType='toppings' />);
+
+  // find images
+  const toppingsImages = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+  expect(toppingsImages).toHaveLength(3);
+
+  // confirm the alt text of images
+  const altText = toppingsImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
