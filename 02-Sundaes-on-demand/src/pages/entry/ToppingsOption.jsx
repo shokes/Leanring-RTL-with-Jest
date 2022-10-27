77b7c0e3c86ce3d25d6bc037cc2e5ea0026fe084
Oshoke <oyatioshoke@yahoo.com>
2022-10-27 11:34:12 +0100

    more tests

--- 02-Sundaes-on-demand/src/pages/entry/ToppingsOption.jsx
+++ 02-Sundaes-on-demand/src/pages/entry/ToppingsOption.jsx
@@ -1,15 +1,23 @@
-import Col from 'react-bootstrap/col';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 export default function ToppingsOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0, 'toppings');
+  };
+
   return (
-    <div>
-      <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
-        <img
-          style={{ width: '75%' }}
-          alt={`${name} topping`}
-          src={`http://localhost3030${imagePath}`}
-        />
-      </Col>
-    </div>
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: 'center' }}>
+      <img
+        style={{ width: '75%' }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} topping`}
+      />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type='checkbox' onChange={handleChange} label={name} />
+      </Form.Group>
+    </Col>
   );
 }
--- 02-Sundaes-on-demand/src/pages/entry/tests/TotalUpdates.test.jsx
+++ 02-Sundaes-on-demand/src/pages/entry/tests/TotalUpdates.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
+import OrderEntry from '../OrderEntry';
 
 test('update scoop subtotal when scoop changes', async () => {
   const user = userEvent.setup();
@@ -63,4 +64,84 @@ test('for toppings subtotal', async () => {
   expect(toppingsSubtotal).toHaveTextContent('1.50');
   await user.click(cherriesCheckbox);
   expect(toppingsSubtotal).toHaveTextContent('3.00');
+});
+
+describe('grand total', () => {
+  test('grand total starts at $0.00', () => {
+    render(<OrderEntry />);
+    const grandTotal = screen.getByText('Grand total: $', {
+      exact: false,
+    });
+
+    expect(grandTotal).toHaveTextContent('0.00');
+  });
+
+  test('grand total updates properly if scoop is added first', async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry />);
+    const chocolateInput = await screen.findByRole('spinbutton', {
+      name: 'Chocolate',
+    });
+
+    await user.clear(chocolateInput);
+    await user.type(chocolateInput, '2');
+    const grandTotal = screen.getByText('Grand total: $', {
+      exact: false,
+    });
+    expect(grandTotal).toHaveTextContent('4.00');
+
+    const Mcheckbox = await screen.findByRole('checkbox', {
+      name: 'M&Ms',
+    });
+
+    await user.clear(Mcheckbox);
+    await user.click(Mcheckbox);
+
+    expect(grandTotal).toHaveTextContent('5.50');
+  });
+
+  test('grand total updates properly if topping is added first', async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry />);
+    const Mcheckbox = await screen.findByRole('checkbox', {
+      name: 'M&Ms',
+    });
+
+    await user.clear(Mcheckbox);
+    await user.click(Mcheckbox);
+
+    const chocolateInput = await screen.findByRole('spinbutton', {
+      name: 'Chocolate',
+    });
+
+    await user.clear(chocolateInput);
+    await user.type(chocolateInput, '2');
+
+    const grandTotal = screen.getByText('Grand total: $', {
+      exact: false,
+    });
+
+    expect(grandTotal).toHaveTextContent('5.50');
+  });
+
+  test('grand total to update properly if item is removed', async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry />);
+    const chocolateInput = await screen.findByRole('spinbutton', {
+      name: 'Chocolate',
+    });
+
+    await user.clear(chocolateInput);
+    await user.type(chocolateInput, '2');
+
+    const grandTotal = screen.getByText('Grand total: $', {
+      exact: false,
+    });
+
+    expect(grandTotal).toHaveTextContent('4.00');
+
+    await user.clear(chocolateInput);
+
+    expect(grandTotal).toHaveTextContent('0.00');
+  });
 });
